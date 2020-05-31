e4846d1c7b7d8d615e349234416fe860e1141cf4
DeSmailer <60794239+DeSmailer@users.noreply.github.com>
2020-05-31 17:10:19 +0300

    всплывающая форма входа

--- header.php
+++ header.php
@@ -112,22 +112,23 @@
                 </div>
             </div>
         </div>
+
+        <!--кнопка открытия модального окна-->
+
+
         <!--профиль-->
         <?php
-        if(!isset($_COOKIE['userId']))
-        {
+        if (!isset($_COOKIE['userId'])) {
             //<a href="registration.php"> или выпадающие окна
             echo "
-            <a href=\"registration.php\">
-            <div class=\"round-sides dark-background\"
-                 style=\"min-width: 100px; padding-left: 10px; color: #FEC541\" align=\"right\">
-                мое очень длинное имя
-                <img src=\"img/user.png\" alt=\"фото\" class=\"rounded-circle img-fluid\" style=\"max-height: 40px\">
-            </div>
+            
+            <button type=\"button\" class=\"btn round-sides dark-background\" data-toggle=\"modal\" data-target=\"#exampleModal\"
+                data-whatever=\"@getbootstrap\" style=\"min-width: 100px; padding: 0; padding-left: 10px;border: 0; color: #FEC541\" align=\"right\">гость
+            <img src=\"img/user.png\" alt=\"фото\" class=\"rounded-circle img-fluid\" style=\"max-height: 40px\">
+        </button>
         </a>
             ";
-        }
-        else{
+        } else {
             //<a href="index.php"> -> кабинет личный
             echo "
             <a href=\"myprofile.php\">
@@ -143,3 +144,125 @@
 
     </nav>
 </header>
+<!-- Button trigger modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog " role="document" style="max-width: 460px">
+        <div class="modal-content orange-background">
+            <div class="modal-header" style="border: 0">
+                <h5 class="modal-title" id="exampleModalLabel">Войти</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" >
+                <!--форма входа-->
+                <form action="create_new_account.php" method="post" id="registrationform" class="needs-validation">
+
+                    <div class="form-group" style="width: 64%;margin-left: 17%;margin-right: 17%">
+                        <input type="text" class="form-control border border-dark form-control-orange" id="email"
+                               name="email"
+                               placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                               title="Неверный адресс эл. почты" required/>
+                    </div>
+                    <div class="form-group" style="width: 64%;margin-left: 17%;margin-right: 17%">
+                        <input type="password" class="form-control border border-dark form-control-orange"
+                               id="password1" name="password1"
+                               placeholder="Пароль"
+                               pattern="(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*]).{8,16}$"
+                               title="Неверный формат пароля. Необходимо 1 заглавная, 1 строчная, 1 цифра, 1спец символ, длинна 8-16"
+                               required/>
+                    </div>
+                    <div align="center">                    <button class="btn  btn-my-dark-color btn-my-dark-size" type="submit" style="max-width: 300px">Войти</button>
+                    </div>
+                    <div>
+                        <div class="label-or" style="text-align: center; margin:  3% 0 3% 0"> или</div>
+                    </div>
+                    <!-- Google Api -->
+                    <div align="center">
+                        <div class="g-signin2" data-onsuccess="onSignIn" data-theme="light" style="width: 300px"></div>
+                    </div>
+                    <script>
+                        function onSignIn(googleUser) {
+                            // Useful data for your client-side scripts:
+                            var profile = googleUser.getBasicProfile();
+                            var id_token = googleUser.getAuthResponse().id_token;
+                            document.location.href = "google_login.php?id=" + profile.getId() + "&email=" + profile.getEmail() + "&name=" + profile.getGivenName() + "&fname=" + profile.getFamilyName();
+                        }
+                    </script>
+                    <!-- Facebook Api -->
+                    <script>
+
+                        function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+                            console.log('statusChangeCallback');
+                            console.log(response);                   // The current login status of the person.
+                            if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+                                testAPI();
+                            }
+                        }
+
+
+                        function checkLoginState() {               // Called when a person is finished with the Login Button.
+                            FB.getLoginStatus(function (response) {   // See the onlogin handler
+                                statusChangeCallback(response);
+                            });
+                        }
+
+
+                        window.fbAsyncInit = function () {
+                            FB.init({
+                                appId: '857050318136434',
+                                cookie: true,                     // Enable cookies to allow the server to access the session.
+                                xfbml: true,                     // Parse social plugins on this webpage.
+                                version: 'v7.0'           // Use this Graph API version for this call.
+                            });
+
+                            FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+                                statusChangeCallback(response);        // Returns the login status.
+                            });
+                        };
+
+
+                        (function (d, s, id) {                      // Load the SDK asynchronously
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src = "https://connect.facebook.net/en_US/sdk.js";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+
+
+                        function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+                            console.log('Welcome!  Fetching your information.... ');
+                            FB.api('/me', function (response) {
+                                console.log('Successful login for: ' + response.name);
+                                document.getElementById('status').innerHTML =
+                                    'Thanks for logging in, ' + response.name + '! <a href=facebook_login.php?name=' + response.name.replace(" ", "_") + '&email=' + response.email + '</a>';
+                            });
+                        }
+
+                    </script>
+                    <div id="fb-root"></div>
+                    <script async defer crossorigin="anonymous"
+                            src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v7.0"></script>
+                    <div align="center" style="margin-top: 16px">
+                        <fb:login-button class="fb-login-button" scope="public_profile,email" onlogin="checkLoginState();" data-size="large"  data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"
+                                         data-width="300px">
+                        </fb:login-button>
+                    </div>
+                    <div id="status">
+                    </div>
+                </form>
+            </div>
+
+            <div class="modal-footer" style="float: left; margin-bottom: 2%; border: 0">
+                <span style="font-size: 12px">У вас еще нет аккаунта?</span>
+                <div class="btn btn-my-dark-color btn-my-dark-size" style="max-height: 32px; max-width: 180px;"><a
+                            href="registration.php" style="color: #FEC541">зарегестрироваться </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal END-->
