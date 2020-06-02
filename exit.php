ce0fe6d4c9eb3105e9ef3569a410018c3d040f12
Агатін Євген <65392476+Jackrir@users.noreply.github.com>
2020-06-02 15:14:53 +0300

    Delete exit.php

--- exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-setcookie('userId', 12, time() - 3600*24, "/");
-setcookie('userName', 12, time() - 3600*24, "/");
-setcookie('userImage', 12, time() - 3600*24, "/");
-
-header('Location: /index.php');
